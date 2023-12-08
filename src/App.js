18a477ed78281dcc567eda35d00a8df1368a2747
KareemAshrafSaeed <kkaarreemm2002@gmail.com>
2023-12-08 08:37:12 +0200

    Admin Home cards, View Stations table

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import ProtectedRoute from './router/ProtectedRoute'
 import { AuthProvider } from './contexts/AuthContext'
 import './scss/style.scss'
+import AdminHome from './views/pages/AdminHome/AdminHome'
+import ViewStations from './views/pages/AdminHome/ViewStation/ViewStation'
+import AddStation from './views/pages/AdminHome/AddStation/AddStation'
 
 const loading = (
   <div className="pt-3 text-center">
@@ -19,6 +22,8 @@ const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
+
+
 class App extends Component {
   render() {
     return (
@@ -30,7 +35,10 @@ class App extends Component {
               <Route exact path="/register" name="Register Page" element={<Register />} />
               <Route exact path="/404" name="Page 404" element={<Page404 />} />
               <Route exact path="/500" name="Page 500" element={<Page500 />} />
-              <Route path="/admin/*" name="Home" element={<ProtectedRoute guard={"admin"} element={<DefaultLayout />} />} />
+              <Route path="/admin/home" name="Home" /*element={<ProtectedRoute guard={"admin"}*/ element={<AdminHome />} />
+              <Route path="/admin/ViewStations" name="View Stations" element = {<ViewStations/>}/>
+              <Route path="/admin/AddStation" name="Add Station" element = {<AddStation/>}/>
+
             </Routes>
           </AuthProvider>
         </Suspense>
---
+++ src/views/pages/AdminHome/AddStation/AddStation.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import CIcon from '@coreui/icons-react'
+import { cilLockLocked, cilUser } from '@coreui/icons'
+import {
+    CTable,
+    CTableHeaderCell,
+    CTableRow,
+    CTableHead,
+    CTableBody,
+    CTableDataCell
+    
+  } from '@coreui/react'
+
+  const AddStation = () =>
+  {
+    return(
+        <>
+        
+
+        </>
+    )
+  }
+
+  export default AddStation
