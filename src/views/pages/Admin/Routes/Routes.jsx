dd5ce4b5d2283f4d2e265c72cfe036fdb9fd29c5
Amr Mahmoud <amrmgoma6@gmail.com>
2023-12-27 17:42:08 +0200

    added route stations page

--- src/views/pages/Admin/Routes/Routes.jsx
+++ src/views/pages/Admin/Routes/Routes.jsx
@@ -8,22 +8,42 @@ import {
     CTableDataCell, CModal, CModalBody, CModalHeader, CModalTitle, CButton, CFormInput, CFormTextarea, CFormCheck, CFormSelect
 } from '@coreui/react';
 import ApiClient from 'src/ApiClient';
+import CIcon from '@coreui/icons-react';
+import { cilChevronBottom, cilChevronTop, cilDelete, cilTrash } from '@coreui/icons';
 function Routes() {
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
     const [stations, setStations] = useState([]);
     const [routes, setRoutes] = useState([]);
+    const [selectedStationIndex, setSelectedStationIndex] = useState(null);
     const [checkedStations, setCheckedStations] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(-1);
 
     const [name, setName] = useState('');
-    
-    const handleCheckChange = (station, isChecked) => {
-        if (isChecked) {
-            setCheckedStations([...checkedStations, station.id]);
-        } else {
-            setCheckedStations(checkedStations.filter(id => id !== station.id));
+
+    const addCheckedStation = (station) => {
+        // Create a new Set with the existing items and the newly selected value
+        if (selectedStationIndex) {
+            setCheckedStations([...checkedStations, station]);
         }
+
+        setSelectedStationIndex(null);
+    }
+
+    const removeCheckedStation = (station) => {
+        // Create a new Set with the existing items and remove from it the selected value
+        setCheckedStations(checkedStations.filter(_station => _station.id !== station.id));
+    }
+
+    const moveUp = (index) => {
+        const newStations = [...checkedStations];
+        [newStations[index - 1], newStations[index]] = [newStations[index], newStations[index - 1]];
+        setCheckedStations(newStations);
+    }
+    const moveDown = (index) => {
+        const newStations = [...checkedStations];
+        [newStations[index + 1], newStations[index]] = [newStations[index], newStations[index + 1]];
+        setCheckedStations(newStations);
     }
 
     const getTableElements = () => {
@@ -38,13 +58,12 @@ function Routes() {
     const getStations = () => {
         ApiClient.get('admin/stations').then((repsonse) => {
             if (repsonse.data.success) {
-                console.log(repsonse);
                 setStations(repsonse.data.data.stations);
             }
         });
     }
 
-    
+
 
 
 
@@ -66,12 +85,11 @@ function Routes() {
 
                 let route = repsonse.data.data.route;
                 setName(route.name);
-                
-                setCheckedStations(route.stations.map(station => station.id));
+                setCheckedStations(route.stations);
 
-                
 
-                
+
+
                 setModalIsOpen(true);
             }
         });
@@ -104,13 +122,14 @@ function Routes() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const orderedStations = checkedStations.map((ob, index) => ({ id: ob.id, order: index }))
+        let data = {
+            'name': name,
+            'stations': orderedStations
+        };
         // updating
         if (currentIndex != -1) {
-            ApiClient.patch('admin/routes/' + routes[currentIndex].id, {
-                'name': name,
-                'stations': checkedStations
-                
-            }).then((repsonse) => {
+            ApiClient.patch('admin/routes/' + routes[currentIndex].id, data).then((repsonse) => {
                 if (repsonse.data.success) {
                     // refresh table
                     getTableElements();
@@ -123,11 +142,7 @@ function Routes() {
             }).catch(() => { })
         } else {
             //creating
-            ApiClient.post('admin/routes', {
-                'name': name,
-                'stations': checkedStations
-
-            }).then((repsonse) => {
+            ApiClient.post('admin/routes', data).then((repsonse) => {
                 if (repsonse.data.success) {
                     getTableElements();
                 }
@@ -160,38 +175,52 @@ function Routes() {
                                 <label>Name:</label>
                                 <CFormInput value={name} onChange={e => setName(e.target.value)} required />
                             </div>
-                            <div>
-    {stations.map((station, index) => (
-        <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
-            <CFormCheck 
-                id={`checkbox${index}`}
-                label={station.name}
-                checked={checkedStations.includes(station.id)}
-                onChange={(e) => {
-                    handleCheckChange(station, e.target.checked);
-                }}
-            />
-            {checkedStations.includes(station.id) && (
-                <CFormSelect 
-                    aria-label="Default select example"
-                    style={{ marginLeft: '10px', marginTop: '10px',width: '75px' }} // Adjust the size and margin as needed
-                    onChange={(e) => {
-                        handleOrderChange(station, e.target.value);
-                    }}
-                >
-                    {[...Array(checkedStations.length)].map((_, i) => (
-                        <option key={i} value={i+1}>{i+1}</option>
-                    ))}
-                </CFormSelect>
-            )}
-        </div>
-    ))}
-</div>
+
+                            <div className='input-group mt-3'>
+                                <label className='me-2'>Stations </label>
+                                <CFormSelect
+                                    value={selectedStationIndex}
+                                    onChange={(e) => setSelectedStationIndex(e.target.value)}
+                                >
+                                    <option value="">-- SELECT -- </option>
+                                    {stations.map((station, index) => !checkedStations.map(ob => ob['id']).includes(station.id) ? (
+                                        <option key={station.id} value={index}>
+                                            {station.name}
+                                        </option>
+                                    ) : '')}
+                                </CFormSelect>
+                                <CButton color='primary' size='sm' onClick={() => addCheckedStation(stations[selectedStationIndex])}>add</CButton>
+                            </div>
+                            <div className='d-flex flex-column gap-3 mt-3'>
+
+                                {checkedStations.map((station, index) => (
+                                    <div key={index} className='d-flex justify-content-between'>
+                                        <span className='w-50'>{station.name}</span>
+                                        <div className='w-25'>
+                                            {index != 0 &&
+                                                <CButton size='sm' className='me-2' onClick={() => moveUp(index)}>
+                                                    <CIcon icon={cilChevronTop}></CIcon>
+                                                </CButton>
+                                            }
+                                            {index != checkedStations.length - 1 &&
+                                                <CButton size='sm' onClick={() => moveDown(index)}>
+                                                    <CIcon icon={cilChevronBottom}></CIcon>
+                                                </CButton>
+                                            }
+                                        </div>
+                                        <div className='w-25 text-end'>
+                                            <CButton color='danger' size='sm' onClick={() => removeCheckedStation(station)} className='text-white'>
+                                                <CIcon icon={cilTrash}></CIcon>
+                                            </CButton>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
 
 
 
 
-                            
+
                             <div className='d-flex justify-content-center mt-4'>
                                 <CButton color="primary" size='lg' type="submit">Save</CButton>
                             </div>
@@ -201,19 +230,19 @@ function Routes() {
                 <CTable striped>
                     <CTableHead>
                         <CTableRow>
-                            
+
                             <CTableHeaderCell scope="col">Name</CTableHeaderCell>
                             <CTableHeaderCell scope="col">ID</CTableHeaderCell>
 
-                            
+
                         </CTableRow>
                     </CTableHead>
                     <CTableBody>
                         {
                             routes.map((route, index) => (
                                 <CTableRow key={route.id}>
-                                    <CTableDataCell>{route.name}</CTableDataCell> 
-                                    <CTableDataCell>{route.id}</CTableDataCell> 
+                                    <CTableDataCell>{route.name}</CTableDataCell>
+                                    <CTableDataCell>{route.id}</CTableDataCell>
 
                                     <CTableDataCell>
                                         <CButton color="success" className='mx-2' onClick={() => editElement(index)}>Edit</CButton>
